2b14b23a5d06fca6f0af1b8a294c86d1f638db46
cooler_lex4nder <a.zaypkin@g.nsu.ru>
2021-02-13 22:36:45 +0700

    Поправки

--- imgs.py
+++ imgs.py
@@ -2,8 +2,7 @@ import csv
 
 with open('stage3_test.csv', encoding='utf-8') as csv_file, open('imgs.csv', 'w', encoding='utf-8') as filtered:
     reader = csv.DictReader(csv_file)
-    field_names = ['Id', 'Images', 'Title', 'Description', 'Price']
-    writer = csv.DictWriter(filtered, fieldnames=field_names)
+    writer = csv.DictWriter(filtered, fieldnames=reader.fieldnames)
     writer.writeheader()
     for row in reader:
         if len(row['Images'].split(',')) > 3:
